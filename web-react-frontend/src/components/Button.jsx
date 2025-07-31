dcbacf1f7ec0b7b76c5f963c7538ec3f1b9688c7
Alvarechi <218034677+Alvarechii23@users.noreply.github.com>
2025-07-31 01:49:24 -0500

    add animation and update styles on 404 page and global background

--- web-react-frontend/src/components/Button.jsx
+++ web-react-frontend/src/components/Button.jsx
@@ -2,10 +2,10 @@ function Button({ children, onClick, className }) {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-4 sm:py-2 [background:radial-gradient(125%_125%_at_50%_10%,#8FA29F_40%,#3A6A6B_100%)] text-white rounded-3xl font-bold hover:bg-green-600 transition ${className}`}
+      className={`px-4 py-4 sm:py-2 [background:radial-gradient(125%_125%_at_50%_10%,#8FA29F_40%,#3c7677ff_100%)] text-white rounded-3xl font-bold hover:bg-green-600 transition ${className}`}
     >
       {children}
     </button>
   );
 }
-export default Button;
+export default Button; 
