b3340c5c03aa97e0a2588fe5f99c430a67316d06
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2025-08-01 18:01:27 -0500

    feat: improved login, added alert message and animation loading

--- web-react-frontend/src/pages/Login.jsx
+++ web-react-frontend/src/pages/Login.jsx
@@ -4,18 +4,29 @@ import axiosPublic from "../api/axiosPublic";
 import Logo from "../assets/Logo.svg";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import AlertMessage from "../components/AlertMessage";
 
 function Login() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-
+    const [mensajes, setMensajes] = useState("");
+    const [errorVisible, setErrorVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
     const handleLogin = async (e) => {
       e.preventDefault();
+      if (!email || !password) {
+        setMensajes("Por favor, completa todos los campos.");
+        setErrorVisible(true);
+        return;
+      }
+
+
+
       try {
         // Hacemos la llamada a la API a través de nuestra instancia `apiClient`
+        setLoading(true);
         const response = await axiosPublic.post("/auth/login/", {
           email,
           password,
@@ -23,8 +34,13 @@ function Login() {
 
         console.log("Login response:", response.data);
 
+        if(response.status !== 200) {
+          throw new Error("Error al iniciar sesión");
+        }
+
         // La API debería devolver los tokens
         const { accessToken, refreshToken } = response.data;
+
 
         // Guardamos los tokens en el localStorage
         localStorage.setItem("accessToken", accessToken);
@@ -32,8 +48,15 @@ function Login() {
 
         navigate("/");
       } catch (error) {
-          console.error("Error al iniciar sesión:", error);
-          alert("Credenciales incorrectas");
+        console.error("Error al iniciar sesión:", error);
+        if (error.response && error.response.data) {
+          setMensajes(error.response.data.detail || "Error al iniciar sesión.");
+        } else {
+          setMensajes("Error de conexión. Por favor, inténtalo de nuevo.");
+        }
+        setErrorVisible(true);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -46,6 +69,14 @@ function Login() {
                 <img src={Logo} alt="Logo" className="w-24 h-24 mb-4" />
                 <h1 className="text-4xl font-semibold">Bienvenido!</h1>
               </div>
+              <AlertMessage
+                type="danger"
+                title="Error"
+                show={errorVisible}
+                onClose={() => setErrorVisible(false)}
+              >
+                {mensajes}
+              </AlertMessage>
               <div className="divide-y divide-gray-200">
                 <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                   <div className="relative"></div>
@@ -85,7 +116,14 @@ function Login() {
                       className="w-full my-4 onClick={handleLogin}"
                       onClick={handleLogin}
                     >
-                      Ingresar
+                      {loading ? (
+                        <div className="flex items-center space-x-2 justify-center">
+                          <span className="loader border-t-transparent border-white border-2 w-4 h-4 rounded-full animate-spin"></span>
+                          <span>Creando...</span>
+                        </div>
+                      ) : (
+                        "Ingresar"
+                      )}
                     </Button>
                   </div>
                 </div>
