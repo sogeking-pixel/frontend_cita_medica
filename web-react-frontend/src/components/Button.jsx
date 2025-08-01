4aa71088a16a1234ffebc75958d6b4ce71b5c8db
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2025-08-01 16:35:55 -0500

    feat: updated hover at Button componentent

--- web-react-frontend/src/components/Button.jsx
+++ web-react-frontend/src/components/Button.jsx
@@ -2,7 +2,8 @@ function Button({ children, onClick, className }) {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-4 sm:py-2 [background:radial-gradient(125%_125%_at_50%_10%,#8FA29F_40%,#3c7677ff_100%)] text-white rounded-3xl font-bold hover:bg-green-600 transition ${className}`}
+      className={`px-4 py-4 sm:py-2 [background:radial-gradient(125%_125%_at_50%_10%,#8FA29F_40%,#3c7677ff_100%)] text-white rounded-3xl font-bold hover:[background:radial-gradient(125%_125%_at_50%_10%,#7F928F_40%,#2a6466ed_100%)] transition-all duration-1000 ease-in-out
+  ${className}`}
     >
       {children}
     </button>
