1768cbf08316cf619ad19768abb24555728ec019
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2025-08-31 16:49:57 -0500

    Update environment variables, enhance axios configuration, and refine login UI text

--- web-react-frontend/src/components/BackGround.jsx
+++ web-react-frontend/src/components/BackGround.jsx
@@ -1,6 +1,6 @@
 function BackGroud(){
     return (
-      <div className="fixed  inset-0 -z-10 h-full w-full [background:radial-gradient(125%_130%_at_50%_100%,#a6c0bcff_40%,#3e6969ff_100%)]"></div>
+      <div className="fixed  inset-0 -z-10 h-full w-full bg-[#f5f5f5]"></div>
     );
 }
 export default BackGroud;
